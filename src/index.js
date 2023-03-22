517a910663458bf3d9d9988b460a5060fb53b42b
Valeria Antonova <saitriniel@gmail.com>
2023-03-22 18:29:23 +0200

    fix index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
 import {fetchPopularArticles, fetchQueryArticles, fetchCategoryArticles} from './js/fetchArticles'
-
-console.log(fetchPopularArticles())
-
-// console.log(fetchQueryArticles(6, 'ukraine'))
-
-// console.log(fetchCategoryArticles(1, 'arts'))
