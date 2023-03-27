180fd05ca554d7f1bed27a6e990f7ed4fab24b92
Valeria Antonova <saitriniel@gmail.com>
2023-03-27 03:33:13 +0300

    add main darkmode files

--- src/index.js
+++ src/index.js
@@ -15,5 +15,6 @@ import {
 } from './js/fetchArticles';
 import { formEl } from './js/homepage-render';
 import { onInputSubmit } from './js/homepage-render';
+import {onSwitcherClick, onStart, enableAnimation, refs} from './js/themeSwitcher';
 
 formEl.addEventListener('submit', onInputSubmit);
