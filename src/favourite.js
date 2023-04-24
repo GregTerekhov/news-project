662bff718c7bbc81277ab863c321ff24439d687f
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2023-04-24 19:14:06 +0300

    delete ref finc from fav

--- src/favourite.js
+++ src/favourite.js
@@ -286,13 +286,6 @@ function addHaveReadNews(newsArr, cardObj, date, key) {
 //-----------------------------------------------------------------------------------------------------------------------------------
 
 //==================== Сервис Функции ===========================================
-export function onGetLocaleStorageData(key) {
-  try {
-    return JSON.parse(localStorage.getItem(key)); // получаем массив объектов из Локального Хранилища
-  } catch (error) {
-    console.log(error);
-  }
-}
 
 function makeParseJson(stringData) {
   try {
